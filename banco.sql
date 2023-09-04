1b708c6af2fb38373b8821d6618f409ea6c6687f
Jorge Bavaresco <jlbavaresco@gmail.com>
2023-09-04 09:41:22 -0300

    SQL da tabela usuários

--- banco.sql
+++ banco.sql
@@ -54,3 +54,18 @@ join categorias c on p.categoria = c.codigo
 order by p.codigo;
 
 select * from avaliacoes;
+
+-- criação da tabela usuários
+create table usuarios (
+	email varchar(50) not null primary key, 
+	senha varchar(20) not null, 
+	tipo char(1)  not null, 
+	check (tipo = 'A' or tipo = 'U'),
+	telefone varchar(14)  not null, 
+	nome varchar(50) not null
+);
+
+-- inserindo alguns registros na tabela usuários
+insert into usuarios (email, senha, tipo, telefone, nome) 
+values ('jorgebavaresco@ifsul.edu.br', '123456', 'A','(54)99984-4348','Jorge Bavaresco'), 
+('joao@ifsul.edu.br', '123456', 'U','(54)44484-4348','Joao');
