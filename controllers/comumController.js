b66d4979fd6faf3fa0e7d8bebd52f48e01db7dce
Jorge Bavaresco <jlbavaresco@gmail.com>
2023-09-04 10:27:44 -0300

    adicionando o endereço correto na API

--- controllers/comumController.js
+++ controllers/comumController.js
@@ -1,5 +1,5 @@
 const getBemVindo = (request, response) => {
-    response.status(200).send("API do ESHOP <br/> <a href='https://agroexperiment.vercel.app/'>Clique para acessar a interface de usuário</a>");
+    response.status(200).send("API do ESHOP <br/> <a href='https://eshop-beta.vercel.app/'>Clique para acessar a interface de usuário</a>");
 }
 
 module.exports = {
